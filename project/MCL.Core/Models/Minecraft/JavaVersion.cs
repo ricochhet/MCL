7f651c0c12e97f51d9545dfa3fe53e6554436a0e
unknown <jonathums@gmail.com>
2024-04-05 14:57:32 -0500

    add java version model

---
+++ project/MCL.Core/Models/Minecraft/JavaVersion.cs
@@ -0,0 +1,7 @@
+namespace MCL.Core.Models.Minecraft;
+
+public class JavaVersion
+{
+    public string Component {  get; set; }
+    public int MajorVersion { get; set; }
+}
--- project/MCL.Core/Models/Minecraft/MCVersion.cs
+++ project/MCL.Core/Models/Minecraft/MCVersion.cs
@@ -7,4 +7,5 @@ public class MCVersion
     public string URL { get; set; }
     public string Time { get; set; }
     public string ReleaseTime { get; set; }
+    public JavaVersion JavaVersion { get; set; }
 }
