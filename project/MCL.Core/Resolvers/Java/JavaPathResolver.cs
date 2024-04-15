42fc6a94305143dee039f7f406d3e48b44cc04a0
unknown <jonathums@gmail.com>
2024-04-15 16:39:09 -0500

    cleanup name conventions for json

--- project/MCL.Core/Resolvers/Java/JavaPathResolver.cs
+++ project/MCL.Core/Resolvers/Java/JavaPathResolver.cs
@@ -17,10 +17,10 @@ public static class JavaPathResolver
     public static string JavaRuntimeBin(string workingDirectory) => VFS.Combine(workingDirectory, "bin");
 
     public static string DownloadedJavaRuntimeIndexPath(MCLauncherPath launcherPath) =>
-        VFS.Combine(JavaRuntimePath(launcherPath), "java_runtime_index.json");
+        VFS.Combine(JavaRuntimePath(launcherPath), "runtime_manifest.json");
 
     public static string DownloadedJavaRuntimeManifestPath(MCLauncherPath launcherPath, string javaRuntimeVersion) =>
-        VFS.Combine(JavaRuntimePath(launcherPath), javaRuntimeVersion, "java_runtime_manifest.json");
+        VFS.Combine(JavaRuntimePath(launcherPath), javaRuntimeVersion, $"runtime_mainfest-{javaRuntimeVersion}.json");
 
     public static string DownloadedJavaRuntimePath(MCLauncherPath launcherPath, string javaRuntimeVersion) =>
         VFS.Combine(JavaRuntimePath(launcherPath), javaRuntimeVersion);
