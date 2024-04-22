c85ec53c9d853a434257f37d961bec0d6863f1ec
unknown <jonathums@gmail.com>
2024-04-21 19:40:27 -0500

    add license notice to all project files

--- project/MCL.CodeAnalyzers/Analyzers/LineAnalyzer.cs
+++ project/MCL.CodeAnalyzers/Analyzers/LineAnalyzer.cs
@@ -1,7 +1,24 @@
+/*
+ * MCL - Minecraft Launcher
+ * Copyright (C) 2024 MCL Contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text.RegularExpressions;
 using MCL.CodeAnalyzers.Analyzers.Models;
 using MCL.Core.Launcher.Services;
 using MCL.Core.Logger.Enums;
@@ -9,7 +26,7 @@ using MCL.Core.MiniCommon;
 
 namespace MCL.CodeAnalyzers.Analyzers;
 
-public static partial class LineAnalyzer
+public static class LineAnalyzer
 {
     public static void Analyze(string[] files)
     {
@@ -38,7 +55,4 @@ public static partial class LineAnalyzer
             Math.Round(fileLines.Average()).ToString()
         );
     }
-
-    [GeneratedRegex(@"(?<=namespace).*?;")]
-    private static partial Regex NamespaceRegex();
 }
